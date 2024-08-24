f0c9058eab9ca4d2d0f5d12e6812eb361688b6b9
Alan González <130946499+ingAlanGlez@users.noreply.github.com>
2024-08-24 03:28:49 -0600

    yml upd

--- src/main/java/com/unir/gateway/GatewayApplication.java
+++ src/main/java/com/unir/gateway/GatewayApplication.java
@@ -10,8 +10,8 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		// Retrieve execution profile from environment variable. Otherwise, default profile is used.
-		String profile = System.getenv("PROFILE");
-		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		// String profile = System.getenv("PROFILE");
+		// System.setProperty("spring.profiles.active", profile != null ? profile : "default");
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
